a1357245def6df5d180fb7a90c66ca9aae56ff27
takahiro-kosaka <takahiro.kosaka@dena.com>
2014-09-22 22:03:17 +0900

    fix android build

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -9,7 +9,7 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 FILE_LIST := $(wildcard $(LOCAL_PATH)/../../../Classes/*.cpp)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/../../../UnicornEngine/*.cpp)
 
-LOCAL_SRC_FILES := hellocpp/main.cpp
+LOCAL_SRC_FILES := main.cpp
 LOCAL_SRC_FILES += $(FILE_LIST:$(LOCAL_PATH)/%=%)
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
